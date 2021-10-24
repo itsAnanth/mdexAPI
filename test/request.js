e4a6c45a43191bc38720623037cfa51f3152bf2b
EJBEAN2op <danonymousarmy@gmail.com>
2021-10-24 14:42:29 +0530

    feat : testing

--- test/request.js
+++ test/request.js
@@ -1,4 +1,14 @@
 import Request from "../src/modules/Request.js";
+import Manga from '../src/structures/Manga.js';
+import Auth from '../src/structures/Auth.js';
+import { config } from 'dotenv';
 
+config();
 
-Request.request('/auth/login', 'POST', { username: 'EJBEAN', password: '[REDACTED]' });
+(async function () {
+    console.log(process.env.USER, process.env.PASSWORD);
+    await Auth.login(process.env.USER, process.env.PASSWORD, true);
+
+    // const res = await Manga.get('a96676e5-8ae2-425e-b549-7f15dd34a6d8');
+    // console.log(res);
+})();
