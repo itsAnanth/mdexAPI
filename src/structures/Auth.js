dd1da04d146dbb177c9e2d8745205ee51b5d69dc
EJBEAN2op <danonymousarmy@gmail.com>
2021-10-24 14:42:13 +0530

    Auth class

--- src/structures/Auth.js
+++ src/structures/Auth.js
@@ -1,9 +1,27 @@
-
+import Request from "../modules/Request.js"
+import Logger from '../modules/Logger.js';
 
 class Auth {
     
-    static async login(username, password) {
-        if (!username || !password) throw new Error('Invalid credentials');
+    /**
+     * 
+     * @param {String} username 
+     * @param {String} password 
+     * @returns {Promise<Object>}
+     */
+    static async login(username, password, DEBUG = false) {
+        if (
+            !username || typeof username != 'string' ||
+            !password || typeof password != 'string'
+        ) throw new Error('Invalid credential(s)');
+
+        const auth = await Request.request('/auth/login', 'POST', { username: username, password: password });
+        if (auth.result == 'ok' && ('token') in auth) {
+            Logger.log('API auth', 'Authorized');
+        }
         
+        return DEBUG ? auth : { success: true };
     }
 }
+
+export default Auth;
